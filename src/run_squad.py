e064bde3c02deb0c87f9c48fe9e0b7df4f52b19b
VinACE <vinsent.storagea@gmail.com>
2022-10-12 18:31:27 +0530

    imported Tuple

--- src/run_squad.py
+++ src/run_squad.py
@@ -21,7 +21,7 @@ import logging
 import os
 import sys
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Optional,Tuple
 
 import datasets
 from datasets import load_dataset, load_metric
